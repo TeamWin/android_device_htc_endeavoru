ce597d2154561c18bf9b7cdb74b395ee83d14f91
DevUt <javitri61@gmail.com>
2016-07-28 22:33:54 +0530

    TWRP RE-ADJUST

--- endeavoru.mk
+++ endeavoru.mk
@@ -56,7 +56,7 @@ PRODUCT_PACKAGES += \
     librs_jni
 
 # Bluetooth tools
-$(call inherit-product, device/htc/endeavoru/hardware/ti/wpan/ti-wpan-products.mk)
+#$(call inherit-product, device/htc/endeavoru/hardware/ti/wpan/ti-wpan-products.mk)
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/prebuilt/bin/load-bt.sh:system/bin/load-bt.sh
 PRODUCT_PACKAGES += \
     l2ping \
@@ -70,7 +70,7 @@ PRODUCT_PACKAGES += \
     tinycap
 
 # Wi-Fi
-$(call inherit-product, device/htc/endeavoru/hardware/ti/wlan/mac80211/wl128x-wlan-products.mk)
+#$(call inherit-product, device/htc/endeavoru/hardware/ti/wlan/mac80211/wl128x-wlan-products.mk)
 PRODUCT_COPY_FILES +=
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/prebuilt/bin/wifi_calibration.sh:system/bin/wifi_calibration.sh \
@@ -85,7 +85,7 @@ PRODUCT_PACKAGES += \
     wlconf
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
-$(call inherit-product, vendor/htc/endeavoru/endeavoru-vendor.mk)
+#$(call inherit-product, vendor/htc/endeavoru/endeavoru-vendor.mk)
 
 # Prebuilt kernel
 ifeq ($(TARGET_PREBUILT_KERNEL),)
--- hardware/ti/wpan/Android.mk
+++
@@ -1,4 +0,0 @@
-#wpan utilties and TI ST user space manager
-ifneq ($(BOARD_HAVE_BLUETOOTH_TI),)
-include $(call first-makefiles-under,$(call my-dir))
-endif
